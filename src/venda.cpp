6213a6bef646dee7ab0dfd55cdaeed42cec5a8a3
Replit user <>
2022-09-29 22:40:08 +0000

    chore: edited files

---
+++ src/venda.cpp
@@ -0,0 +1,21 @@
+#include "venda.hpp"
+
+int Venda::getId()
+{
+  return _id;
+}
+
+void Venda::setId(int id)
+{
+  _id = id;
+}
+int Venda::getQuantidade()
+{
+  return _quantidade;
+}
+
+void Venda::setQuantidade(int qnt)
+{
+  quantidade = qnt;
+
+}
