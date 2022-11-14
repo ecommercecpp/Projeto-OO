6058746960b3fc8924e1b349c439fb9891dee2df
EduScf <eduardoscf1210@gmail.com>
2022-11-14 16:22:42 -0300

    docs: Doxygen ordemProducao.cpp
    Implementação do doxygen da classe ordemProducao

--- src/ordemProducao.cpp
+++ src/ordemProducao.cpp
@@ -2,53 +2,122 @@
 
 #include "ordemProducao.hpp"
 
-OrdemProducao::OrdemProducao(){
-
+/**
+ * @brief Construct a new Ordem Producao:: Ordem Producao object
+ *
+ */
+OrdemProducao::OrdemProducao()
+{
 }
 
-OrdemProducao::OrdemProducao(Lote lote, Data data, int quantidade){
+/**
+ * @brief Construct a new Ordem Producao:: Ordem Producao object
+ *
+ * @param lote
+ * @param data
+ * @param quantidade
+ */
+OrdemProducao::OrdemProducao(Lote lote, Data data, int quantidade)
+{
     setLote(lote);
     setData(data);
     setQuantidade(quantidade);
 }
 
-OrdemProducao::~OrdemProducao(){
-
+/**
+ * @brief Destroy the Ordem Producao:: Ordem Producao object
+ *
+ */
+OrdemProducao::~OrdemProducao()
+{
 }
 
-void OrdemProducao::setCodigo(int codigo){
+/**
+ * @brief Seta codigo do lote
+ *
+ * @param codigo
+ */
+void OrdemProducao::setCodigo(int codigo)
+{
     this->codigo = codigo;
 }
 
-int OrdemProducao::getCodigo(){
+/**
+ * @brief Retorna o codigo da ordem de produção
+ *
+ * @return int
+ */
+int OrdemProducao::getCodigo()
+{
     return codigo;
 }
 
-void OrdemProducao::setLote(Lote lote){
+/**
+ * @brief Seta o lote da ordem de produção
+ *
+ * @param lote
+ */
+void OrdemProducao::setLote(Lote lote)
+{
     this->lote = lote;
 }
 
-void OrdemProducao::setData(Data data){
+/**
+ * @brief Seta a data da ordem de produção
+ *
+ * @param data
+ */
+void OrdemProducao::setData(Data data)
+{
     this->dataDaOrdem = data;
 }
 
-Lote OrdemProducao::getLote(){
+/**
+ * @brief Retorna o lote da ordem de produção
+ *
+ * @return Lote
+ */
+Lote OrdemProducao::getLote()
+{
     return lote;
 }
 
-Data OrdemProducao::getData(){
+/**
+ * @brief Retorna a data da ordem de produção
+ *
+ * @return Data
+ */
+Data OrdemProducao::getData()
+{
     return dataDaOrdem;
 }
 
-int OrdemProducao::getQuantidade(){
+/**
+ * @brief Retorna a quantidade da ordem de produção
+ *
+ * @return int
+ */
+int OrdemProducao::getQuantidade()
+{
     return quantidade;
 }
 
-void OrdemProducao::setQuantidade(int quantidade){
+/**
+ * @brief Seta a quantidade da ordem de produção
+ *
+ * @param quantidade
+ */
+void OrdemProducao::setQuantidade(int quantidade)
+{
     this->quantidade = quantidade;
 }
 
-void OrdemProducao::imprimeOrdem(){
+/**
+ * @brief Imprime na tela os dados da ordem de produção
+ *
+ */
+void OrdemProducao::imprimeOrdem()
+{
     std::cout << "Ordem de produção: " << std::endl;
     std::cout << "Lote: " << getLote().getNmrLote() << std::endl;
     std::cout << "Data: " << getData().getDia() << "/" << getData().getMes() << "/" << getData().getAno() << std::endl;
