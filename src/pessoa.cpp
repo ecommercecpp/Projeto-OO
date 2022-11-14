f4a1514ba113480d0485d7ccb618cc9d9f037f67
EduScf <eduardoscf1210@gmail.com>
2022-11-14 16:36:28 -0300

    docs: Doxygen pessoa.cpp
    Implementação do doxygen da classe pessoa

--- src/pessoa.cpp
+++ src/pessoa.cpp
@@ -3,19 +3,35 @@
 
 #include <iostream>
 
+/**
+ * @brief Construct a new Pessoa:: Pessoa object
+ *
+ */
 Pessoa::Pessoa()
 {
 }
 
-Pessoa::Pessoa(std::string nome, std::string cpf_cnpj, std::string endereco, std::string email, int tipo) :
-	nome(nome), endereco(endereco)
+/**
+ * @brief Construct a new Pessoa:: Pessoa object
+ *
+ * @param nome
+ * @param cpf_cnpj
+ * @param endereco
+ * @param email
+ * @param tipo
+ */
+Pessoa::Pessoa(std::string nome, std::string cpf_cnpj, std::string endereco, std::string email, int tipo) : nome(nome), endereco(endereco)
 {
 	setTipo(tipo);
 	setcpf_cnpj(cpf_cnpj);
 	setEmail(email);
 }
 
-
+/**
+ * @brief Seta o cpf ou cnpj da pessoa
+ *
+ * @param cpf_cnpj
+ */
 void Pessoa::setcpf_cnpj(std::string cpf_cnpj)
 {
 	if (tipo == 0)
@@ -42,6 +58,11 @@ void Pessoa::setcpf_cnpj(std::string cpf_cnpj)
 	}
 }
 
+/**
+ * @brief Seta o email da pessoa
+ *
+ * @param email
+ */
 void Pessoa::setEmail(std::string email)
 {
 	if (email.find("@") != std::string::npos)
@@ -54,6 +75,11 @@ void Pessoa::setEmail(std::string email)
 	}
 }
 
+/**
+ * @brief Seta o tipo da pessoa
+ *
+ * @param tipo
+ */
 void Pessoa::setTipo(int tipo)
 {
 	// nao permite tipos que não sejam 0 ou 1
@@ -67,36 +93,71 @@ void Pessoa::setTipo(int tipo)
 	}
 }
 
+/**
+ * @brief Retorna o nome da pessoa
+ *
+ * @return std::string
+ */
 std::string Pessoa::getNome()
 {
 	return nome;
 }
 
+/**
+ * @brief Retorna o cpf ou cnpj da pessoa
+ *
+ * @return std::string
+ */
 std::string Pessoa::getCpf_cnpj()
 {
 	return cpf_cnpj;
 }
 
+/**
+ * @brief Retorna o endereco da pessoa
+ *
+ * @return std::string
+ */
 std::string Pessoa::getEndereco()
 {
 	return endereco;
 }
 
+/**
+ * @brief Retorna o email da pessoa
+ *
+ * @return std::string
+ */
 std::string Pessoa::getEmail()
 {
 	return email;
 }
 
+/**
+ * @brief Retorna o tipo da pessoa
+ *
+ * @return int
+ */
 int Pessoa::getTipo()
 {
 	return tipo;
 }
 
+/**
+ * @brief Seta o nome da pessoa
+ *
+ * @param nome
+ */
 void Pessoa::setNome(std::string nome)
 {
 	this->nome = nome;
 }
 
+/**
+ * @brief Seta o endereco da pessoa
+ *
+ * @param endereco
+ */
 void Pessoa::setEndereco(std::string endereco)
 {
 	this->endereco = endereco;
