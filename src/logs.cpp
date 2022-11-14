6c574fd5f9df437e553ac309254f63c1a8d98462
EduScf <eduardoscf1210@gmail.com>
2022-11-14 16:08:11 -0300

    docs: Doxygen logs.cpp
    Implementação do doxygen da classe logs

--- src/logs.cpp
+++ src/logs.cpp
@@ -1,13 +1,28 @@
 #include "logs.hpp"
 
+/**
+ * @brief Construct a new Logs:: Logs object
+ * 
+ */
 Logs::Logs()
 {	
 }
 
+/**
+ * @brief Destroy the Logs:: Logs object
+ * 
+ */
 Logs::~Logs()
 {
 }
 
+/**
+ * @brief Construct a new Logs:: Logs object
+ * 
+ * @param usuario 
+ * @param data 
+ * @param entidade 
+ */
 Logs::Logs(Usuario usuario, Data data, std::string entidade)
 {
     this->usuario = usuario;
@@ -15,32 +30,61 @@ Logs::Logs(Usuario usuario, Data data, std::string entidade)
     this->entidade = entidade;
 }
 
-
+/**
+ * @brief Seta o usuario do log
+ * 
+ * @param usuario 
+ */
 void Logs::setUsuario(Usuario usuario)
 {
     this->usuario = usuario;
 }
 
+/**
+ * @brief Retorna o usuario do log
+ * 
+ * @return Usuario 
+ */
 Usuario Logs::getUsuario()
 {
     return this->usuario;
 }
 
+/**
+ * @brief Seta a data do log
+ * 
+ * @param data 
+ */
 void Logs::setData(Data data)
 {
     this->data = data;
 }
 
+/**
+ * @brief Retorna a data do log
+ * 
+ * @return Data 
+ */
 Data Logs::getData()
 {
     return this->data;
 }
 
+/**
+ * @brief Seta a entidade do log
+ * 
+ * @param entidade 
+ */
 void Logs::setEntidade(std::string entidade)
 {
     this->entidade = entidade;
 }
 
+/**
+ * @brief Retorna a entidade do log
+ * 
+ * @return std::string 
+ */
 std::string Logs::getEntidade()
 {
     return this->entidade;
