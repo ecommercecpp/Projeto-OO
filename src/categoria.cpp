f63d2f3e94ae131d84f5d29e35a5cf76e4d4d1d1
EduScf <eduardoscf1210@gmail.com>
2022-11-14 13:38:30 -0300

    docs: Doxygen Categoria
    Implementação do Doxygen da Classe Categoria

--- src/categoria.cpp
+++ src/categoria.cpp
@@ -2,24 +2,47 @@
 
 #include "categoria.hpp"
 
+/**
+ * @brief Construct a new Categoria::Categoria object
+ *
+ */
 Categoria::Categoria()
 {
 }
 
+/**
+ * @brief Construct a new Categoria::Categoria object
+ *
+ * @param nome
+ */
 Categoria::Categoria(std::string nome)
 {
     setNome(nome);
 }
 
+/**
+ * @brief Destroy the Categoria:: Categoria object
+ *
+ */
 Categoria::~Categoria()
 {
 }
 
+/**
+ * @brief Retorna o nome da categoria
+ *
+ * @return std::string
+ */
 std::string Categoria::getNome()
 {
     return nome;
 }
 
+/**
+ * @brief Seta o nome da categoria
+ *
+ * @param nome
+ */
 void Categoria::setNome(std::string nome)
 {
     this->nome = nome;
