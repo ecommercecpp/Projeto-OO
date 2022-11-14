b29aa9eea0e0a541db4a344ef373e7af2231ec38
EduScf <eduardoscf1210@gmail.com>
2022-11-14 13:58:00 -0300

    docs: doxygen empresa.cpp
    Implementado o doxygen da classe Empresa

--- src/empresa.cpp
+++ src/empresa.cpp
@@ -2,156 +2,337 @@
 
 #include "empresa.hpp"
 
+/**
+ * @brief Construct a new Empresa:: Empresa singleton object
+ *
+ */
 Empresa *Empresa::empresa = new Empresa();
 
+/**
+ * @brief Construct a new Empresa:: Empresa object
+ *
+ */
 Empresa::Empresa()
 {
 }
 
+/**
+ * @brief Retorna a instancia da empresa
+ *
+ * @return Empresa*
+ */
 Empresa *Empresa::getEmpresa()
 {
-  return empresa;
+    return empresa;
 }
 
-void Empresa::adicionarDepartamento(Departamento* departamento){
+/**
+ * @brief Adiciona um departamento a empresa
+ *
+ * @param departamento
+ */
+void Empresa::adicionarDepartamento(Departamento *departamento)
+{
     departamentos.push_back(departamento);
 }
 
-void Empresa::removerDepartamento(Departamento* departamento){
-    for (unsigned int i = 0; i < departamentos.size(); i++){
-        if (departamentos[i] == departamento){
+/**
+ * @brief Remove um departamento da empresa
+ *
+ * @param departamento
+ */
+void Empresa::removerDepartamento(Departamento *departamento)
+{
+    for (unsigned int i = 0; i < departamentos.size(); i++)
+    {
+        if (departamentos[i] == departamento)
+        {
             departamentos.erase(departamentos.begin() + i);
         }
     }
 }
 
-void Empresa::adicionarCargo(Cargo* cargo){
+/**
+ * @brief Adiciona um cargo a empresa
+ *
+ * @param cargo
+ */
+void Empresa::adicionarCargo(Cargo *cargo)
+{
     cargos.push_back(cargo);
 }
 
-void Empresa::removerCargo(Cargo* cargo){
-    for (unsigned int i = 0; i < cargos.size(); i++){
-        if (cargos[i] == cargo){
+/**
+ * @brief Remove um cargo da empresa
+ *
+ * @param cargo
+ */
+void Empresa::removerCargo(Cargo *cargo)
+{
+    for (unsigned int i = 0; i < cargos.size(); i++)
+    {
+        if (cargos[i] == cargo)
+        {
             cargos.erase(cargos.begin() + i);
         }
     }
 }
 
-void Empresa::adicionarFuncionario(Funcionario* funcionario){
+/**
+ * @brief Adiciona um funcionario a empresa
+ *
+ * @param funcionario
+ */
+void Empresa::adicionarFuncionario(Funcionario *funcionario)
+{
     funcionarios.push_back(funcionario);
 }
 
-void Empresa::removerFuncionario(Funcionario* funcionario){
-    for (unsigned int i = 0; i < funcionarios.size(); i++){
-        if (funcionarios[i] == funcionario){
+/**
+ * @brief Remove um funcionario da empresa
+ *
+ * @param funcionario
+ */
+void Empresa::removerFuncionario(Funcionario *funcionario)
+{
+    for (unsigned int i = 0; i < funcionarios.size(); i++)
+    {
+        if (funcionarios[i] == funcionario)
+        {
             funcionarios.erase(funcionarios.begin() + i);
         }
     }
 }
 
-void Empresa::adicionarCliente(Cliente* cliente){
+/**
+ * @brief Adiciona um cliente a empresa
+ *
+ * @param cliente
+ */
+void Empresa::adicionarCliente(Cliente *cliente)
+{
     clientes.push_back(cliente);
 }
 
-void Empresa::removerCliente(Cliente* cliente){
-    for (unsigned int i = 0; i < clientes.size(); i++){
-        if (clientes[i] == cliente){
+/**
+ * @brief Remove um cliente da empresa
+ *
+ * @param cliente
+ */
+void Empresa::removerCliente(Cliente *cliente)
+{
+    for (unsigned int i = 0; i < clientes.size(); i++)
+    {
+        if (clientes[i] == cliente)
+        {
             clientes.erase(clientes.begin() + i);
         }
     }
 }
 
-void Empresa::adicionarOrdemProducao(OrdemProducao* ordemProducao){
+/**
+ * @brief Adiciona uma ordem de produção a empresa
+ *
+ * @param ordemProducao
+ */
+void Empresa::adicionarOrdemProducao(OrdemProducao *ordemProducao)
+{
     ordemProducaoLista.push_back(ordemProducao);
 }
 
-void Empresa::removerOrdemProducao(OrdemProducao* ordemProducao){
-    for (unsigned int i = 0; i < ordemProducaoLista.size(); i++){
-        if (ordemProducaoLista[i] == ordemProducao){
+/**
+ * @brief Remove uma ordem de produção da empresa
+ *
+ * @param ordemProducao
+ */
+void Empresa::removerOrdemProducao(OrdemProducao *ordemProducao)
+{
+    for (unsigned int i = 0; i < ordemProducaoLista.size(); i++)
+    {
+        if (ordemProducaoLista[i] == ordemProducao)
+        {
             ordemProducaoLista.erase(ordemProducaoLista.begin() + i);
         }
     }
 }
 
-void Empresa::adicionarProduto(Produto* produto){
+/**
+ * @brief Adiciona um produto a empresa
+ *
+ * @param produto
+ */
+void Empresa::adicionarProduto(Produto *produto)
+{
     produtos.push_back(produto);
 }
 
-void Empresa::removerProduto(Produto* produto){
-    for (unsigned int i = 0; i < produtos.size(); i++){
-        if (produtos[i] == produto){
+/**
+ * @brief Remove um produto da empresa
+ *
+ * @param produto
+ */
+void Empresa::removerProduto(Produto *produto)
+{
+    for (unsigned int i = 0; i < produtos.size(); i++)
+    {
+        if (produtos[i] == produto)
+        {
             produtos.erase(produtos.begin() + i);
         }
     }
 }
 
-void Empresa::adicionarCategoria(Categoria* categoria){
+/**
+ * @brief Adiciona uma categoria a empresa
+ *
+ * @param categoria
+ */
+void Empresa::adicionarCategoria(Categoria *categoria)
+{
     categorias.push_back(categoria);
 }
 
-void Empresa::removerCategoria(Categoria* categoria){
-    for (unsigned int i = 0; i < categorias.size(); i++){
-        if (categorias[i] == categoria){
+/**
+ * @brief Remove uma categoria da empresa
+ *
+ * @param categoria
+ */
+void Empresa::removerCategoria(Categoria *categoria)
+{
+    for (unsigned int i = 0; i < categorias.size(); i++)
+    {
+        if (categorias[i] == categoria)
+        {
             categorias.erase(categorias.begin() + i);
         }
     }
 }
 
-void Empresa::adicionarLote(Lote* lote){
+/**
+ * @brief Adiciona um lote a empresa
+ *
+ * @param lote
+ */
+void Empresa::adicionarLote(Lote *lote)
+{
     lotes.push_back(lote);
 }
 
-void Empresa::removerLote(Lote* lote){
-    for (unsigned int i = 0; i < lotes.size(); i++){
-        if (lotes[i] == lote){
+/**
+ * @brief Remove um lote da empresa
+ *
+ * @param lote
+ */
+void Empresa::removerLote(Lote *lote)
+{
+    for (unsigned int i = 0; i < lotes.size(); i++)
+    {
+        if (lotes[i] == lote)
+        {
             lotes.erase(lotes.begin() + i);
         }
     }
 }
 
-void Empresa::adicionarVenda(Venda* venda){
+/**
+ * @brief Adiciona uma venda a empresa
+ *
+ * @param venda
+ */
+void Empresa::adicionarVenda(Venda *venda)
+{
     vendas.push_back(venda);
 }
 
-void Empresa::removerVenda(Venda* venda){
-    for (unsigned int i = 0; i < vendas.size(); i++){
-        if (vendas[i] == venda){
+/**
+ * @brief Remove uma venda da empresa
+ *
+ * @param venda
+ */
+void Empresa::removerVenda(Venda *venda)
+{
+    for (unsigned int i = 0; i < vendas.size(); i++)
+    {
+        if (vendas[i] == venda)
+        {
             vendas.erase(vendas.begin() + i);
         }
     }
 }
 
-void Empresa::adicionarLogEscrita(LogEscrita* logEscrita){
+/**
+ * @brief Adiciona um Log de escrita a empresa
+ *
+ * @param LogEscrita
+ */
+void Empresa::adicionarLogEscrita(LogEscrita *logEscrita)
+{
     logsEscrita.push_back(logEscrita);
 }
 
-void Empresa::removerLogEscrita(LogEscrita* logEscrita){
-    for (unsigned int i = 0; i < logsEscrita.size(); i++){
-        if (logsEscrita[i] == logEscrita){
+/**
+ * @brief Remove um Log de escrita da empresa
+ *
+ * @param LogEscrita
+ */
+void Empresa::removerLogEscrita(LogEscrita *logEscrita)
+{
+    for (unsigned int i = 0; i < logsEscrita.size(); i++)
+    {
+        if (logsEscrita[i] == logEscrita)
+        {
             logsEscrita.erase(logsEscrita.begin() + i);
         }
     }
 }
 
-void Empresa::adicionarLogLeitura(LogLeitura* logLeitura){
+/**
+ * @brief Adiciona um Log de leitura a empresa
+ *
+ * @param LogLeitura
+ */
+void Empresa::adicionarLogLeitura(LogLeitura *logLeitura)
+{
     logsLeitura.push_back(logLeitura);
 }
 
-void Empresa::removerLogLeitura(LogLeitura* logLeitura){
-    for (unsigned int i = 0; i < logsLeitura.size(); i++){
-        if (logsLeitura[i] == logLeitura){
+/**
+ * @brief Remove um Log de leitura da empresa
+ *
+ * @param LogLeitura
+ */
+void Empresa::removerLogLeitura(LogLeitura *logLeitura)
+{
+    for (unsigned int i = 0; i < logsLeitura.size(); i++)
+    {
+        if (logsLeitura[i] == logLeitura)
+        {
             logsLeitura.erase(logsLeitura.begin() + i);
         }
     }
 }
 
-void Empresa::adicionarLogExcecao(LogExcecao* logExcecao){
+/**
+ * @brief Adiciona um Log de exceção a empresa
+ *
+ * @param LogEcecao
+ */
+void Empresa::adicionarLogExcecao(LogExcecao *logExcecao)
+{
     logsExcecao.push_back(logExcecao);
 }
 
-void Empresa::removerLogExcecao(LogExcecao* logExcecao){
-    for (unsigned int i = 0; i < logsExcecao.size(); i++){
-        if (logsExcecao[i] == logExcecao){
+/**
+ * @brief Remove um Log de exceção da empresa
+ *
+ * @param LogExcecao
+ */
+void Empresa::removerLogExcecao(LogExcecao *logExcecao)
+{
+    for (unsigned int i = 0; i < logsExcecao.size(); i++)
+    {
+        if (logsExcecao[i] == logExcecao)
+        {
             logsExcecao.erase(logsExcecao.begin() + i);
         }
     }
