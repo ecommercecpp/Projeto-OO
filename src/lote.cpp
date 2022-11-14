c108b00267bff63bdf9aa0a960fcb6f090ba043c
EduScf <eduardoscf1210@gmail.com>
2022-11-14 16:11:16 -0300

    docs: Doxygen lote.cpp
    Implementação do doxygen da classe lote

--- src/lote.cpp
+++ src/lote.cpp
@@ -2,10 +2,22 @@
 
 #include "lote.hpp"
 
+/**
+ * @brief Construct a new Lote:: Lote object
+ *
+ */
 Lote::Lote()
 {
 }
 
+/**
+ * @brief Construct a new Lote:: Lote object
+ *
+ * @param quantidade
+ * @param quantidadeMinima
+ * @param nmrLote
+ * @param codigoProduto
+ */
 Lote::Lote(int quantidade, int quantidadeMinima, int nmrLote, int codigoProduto)
 {
     setQuantidade(quantidade);
@@ -14,65 +26,129 @@ Lote::Lote(int quantidade, int quantidadeMinima, int nmrLote, int codigoProduto)
     setCodigoProduto(codigoProduto);
 }
 
+/**
+ * @brief Destroy the Lote:: Lote object
+ *
+ */
 Lote::~Lote()
 {
 }
 
+/**
+ * @brief Seta a quantidade
+ *
+ * @param quantidade
+ */
 void Lote::setQuantidade(int quantidade)
 {
     this->quantidade = quantidade;
 }
 
+/**
+ * @brief Seta a quantidade minima do lote
+ *
+ * @param quantidadeMinima
+ */
 void Lote::setQuantidadeMinima(int quantidadeMinima)
 {
     this->quantidadeMinima = quantidadeMinima;
 }
 
+/**
+ * @brief Seta o numero do lote
+ *
+ * @param nmrLote
+ */
 void Lote::setNmrLote(int nmrLote)
 {
     this->nmrLote = nmrLote;
 }
 
+/**
+ * @brief Seta o codigo do produto
+ *
+ * @param codigoProduto
+ */
 void Lote::setCodigoProduto(int codigoProduto)
 {
     this->codigoProduto = codigoProduto;
 }
 
+/**
+ * @brief Seta a data de produção do lote
+ *
+ * @param dataDeProducao
+ */
 void Lote::setDataDeProducao(Data dataDeProducao)
 {
     this->dataDeProducao = dataDeProducao;
 }
 
+/**
+ * @brief Seta o produto do lote
+ *
+ * @param produto
+ */
 void Lote::setProduto(std::string produto)
 {
     this->produto = produto;
 }
 
+/**
+ * @brief Retorna a quantidade do lote
+ *
+ * @return int
+ */
 int Lote::getQuantidade()
 {
     return quantidade;
 }
 
+/**
+ * @brief Retorna a quantidade minima do lote
+ *
+ * @return int
+ */
 int Lote::getQuantidadeMinima()
 {
     return quantidadeMinima;
 }
 
+/**
+ * @brief Retorna o numero do lote
+ *
+ * @return int
+ */
 int Lote::getNmrLote()
 {
     return nmrLote;
 }
 
+/**
+ * @brief Retorna o codigo do produto
+ *
+ * @return int
+ */
 int Lote::getCodigoProduto()
 {
     return codigoProduto;
 }
 
+/**
+ * @brief Retorna a data de produção do lote
+ *
+ * @return Data
+ */
 Data Lote::getDataDeProducao()
 {
     return dataDeProducao;
 }
 
+/**
+ * @brief Retorna o produto do lote
+ *
+ * @return std::string
+ */
 std::string Lote::getProduto()
 {
     return produto;
