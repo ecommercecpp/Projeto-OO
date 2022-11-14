d13c64ddba0ffee70f32d79d12697a33e9fde64d
EduScf <eduardoscf1210@gmail.com>
2022-11-14 15:42:11 -0300

    docs: Doxygen credito.cpp
    Implementação do doxygen da classe Credito

--- src/credito.cpp
+++ src/credito.cpp
@@ -2,10 +2,22 @@
 
 #include "credito.hpp"
 
+/**
+ * @brief Construct a new Credito::Credito object
+ *
+ */
 Credito::Credito()
 {
 }
 
+/**
+ * @brief Construct a new Credito::Credito object
+ *
+ * @param codigoDeSeguranca
+ * @param nomeDoTitular
+ * @param dataDeVencimento
+ * @param numeroDoCartao
+ */
 Credito::Credito(std::string codigoDeSeguranca, std::string nomeDoTitular, Data dataDeVencimento, std::string numeroDoCartao)
 {
     this->codigoDeSeguranca = codigoDeSeguranca;
@@ -14,41 +26,81 @@ Credito::Credito(std::string codigoDeSeguranca, std::string nomeDoTitular, Data 
     this->numeroDoCartao = numeroDoCartao;
 }
 
+/**
+ * @brief Retorna o codigo de seguranca do cartao de credito
+ *
+ * @return std::string
+ */
 std::string Credito::getCodigoDeSeguranca()
 {
     return this->codigoDeSeguranca;
 }
 
+/**
+ * @brief Seta o codigo de seguranca do cartao de credito
+ *
+ * @param codigoDeSeguranca
+ */
 void Credito::setCodigoDeSeguranca(std::string codigoDeSeguranca)
 {
     this->codigoDeSeguranca = codigoDeSeguranca;
 }
 
+/**
+ * @brief Retorna o nome do titular do cartao de credito
+ *
+ * @return std::string
+ */
 std::string Credito::getNomeDoTitular()
 {
     return this->nomeDoTitular;
 }
 
+/**
+ * @brief Seta o nome do titular do cartao de credito
+ *
+ * @param nomeDoTitular
+ */
 void Credito::setNomeDoTitular(std::string nomeDoTitular)
 {
     this->nomeDoTitular = nomeDoTitular;
 }
 
+/**
+ * @brief Retorna a data de vencimento do credito
+ *
+ * @return Data
+ */
 Data Credito::getDataDeVencimento()
 {
     return this->dataDeVencimento;
 }
 
+/**
+ * @brief Seta a data de vencimento do credito
+ *
+ * @param dataDeVencimento
+ */
 void Credito::setDataDeVencimento(Data dataDeVencimento)
 {
     this->dataDeVencimento = dataDeVencimento;
 }
 
+/**
+ * @brief Retorna o numero do cartao de credito
+ *
+ * @return std::string
+ */
 std::string Credito::getNumeroDoCartao()
 {
     return this->numeroDoCartao;
 }
 
+/**
+ * @brief Seta o numero do cartao de credito
+ *
+ * @param numeroDoCartao
+ */
 void Credito::setNumeroDoCartao(std::string numeroDoCartao)
 {
     this->numeroDoCartao = numeroDoCartao;
