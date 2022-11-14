54d2e7d729877108522e23ca2dff9e9038a64e0e
EduScf <eduardoscf1210@gmail.com>
2022-11-14 16:52:27 -0300

    docs: Doxygen usuarioLogado.cpp
    Implementação do doxygen da classe usuarioLogado

--- src/usuarioLogado.cpp
+++ src/usuarioLogado.cpp
@@ -4,10 +4,19 @@
 
 Usuario *UsuarioLogado::usuario = nullptr;
 
+/**
+ * @brief Construct a new Usuario Logado:: Usuario Logado object
+ *
+ */
 UsuarioLogado::UsuarioLogado()
 {
 }
 
+/**
+ * @brief Retorna a instancia do usuario que está logado (singleton)
+ *
+ * @return UsuarioLogado*
+ */
 UsuarioLogado *UsuarioLogado::GetInstance()
 {
     if (usuario == nullptr)
@@ -16,4 +25,3 @@ UsuarioLogado *UsuarioLogado::GetInstance()
     }
     return (UsuarioLogado *)usuario;
 }
-
