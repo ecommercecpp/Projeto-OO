6f5b1a01abe958f442c827460ada3779b699f136
EduScf <eduardoscf1210@gmail.com>
2022-11-14 13:47:28 -0300

    docs: doxygen departamento.cpp
    Implementação do doxygen da classe Departamento

--- src/departamento.cpp
+++ src/departamento.cpp
@@ -4,24 +4,47 @@
 
 int Departamento::current_id;
 
+/**
+ * @brief Construct a new Departamento::Departamento object
+ *
+ */
 Departamento::Departamento() : id(++current_id) {}
 
-//Departamento::Departamento(std::string nome) : id(++current_id), nome(nome) {}
-
-Departamento::Departamento(std::string nome){
-  this->nome  = nome;
+/**
+ * @brief Construct a new Departamento:: Departamento object
+ *
+ * @param nome
+ */
+Departamento::Departamento(std::string nome)
+{
+	this->nome = nome;
 }
 
+/**
+ * @brief Retorna o Id do Departamento
+ *
+ * @return int
+ */
 int Departamento::getId()
 {
 	return this->id;
 }
 
+/**
+ * @brief Retorna o nome do Departamento
+ *
+ * @return std::string
+ */
 std::string Departamento::getNome()
 {
 	return this->nome;
 }
 
+/**
+ * @brief Seta o Id do Departamento
+ *
+ * @param id
+ */
 void Departamento::setNome(std::string nome)
 {
 	this->nome = nome;
