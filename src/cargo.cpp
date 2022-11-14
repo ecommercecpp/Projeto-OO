7ba3a5b9426198f5f5756c9ef4d2d1809596583d
EduScf <eduardoscf1210@gmail.com>
2022-11-14 13:32:58 -0300

    docs: Doxygen Cargo
    Implementação do Doxygen da classe Cargo

--- src/cargo.cpp
+++ src/cargo.cpp
@@ -2,25 +2,56 @@
 
 int Cargo::current_id;
 
+/**
+ * @brief Construct a new Cargo::Cargo object
+ *
+ */
 Cargo::Cargo() : id(++current_id) {}
+
+/**
+ * @brief Construct a new Cargo::Cargo object
+ *
+ * @param nome
+ * @param current_id
+ */
 
 Cargo::Cargo(std::string nome) : id(++current_id), nome(nome) {}
 
+/**
+ * @brief Retorna o nome do cargo
+ *
+ * @return std::string
+ */
 std::string Cargo::getNome()
 {
 	return nome;
 }
 
+/**
+ * @brief Retorna o id do cargo
+ *
+ * @return int
+ */
 int Cargo::getId()
 {
 	return id;
 }
 
+/**
+ * @brief Seta o nome do cargo
+ *
+ * @param nome
+ */
 void Cargo::setNome(std::string nome)
 {
 	this->nome = nome;
 }
 
+/**
+ * @brief Seta o id do cargo
+ *
+ * @param id
+ */
 void Cargo::setId(int id)
 {
 	this->id = id;
