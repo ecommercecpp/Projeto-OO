3342875e189c7eca3a61945057f75f97f9a2b31f
EduScf <eduardoscf1210@gmail.com>
2022-11-14 15:50:58 -0300

    docs: doxygen Formato.cpp
    Implementação do doxygen da classe Formato

--- src/Formato.cpp
+++ src/Formato.cpp
@@ -8,35 +8,63 @@
 
 using namespace std;
 
+/**
+ * @brief Construct a new Formato:: Formato object
+ *
+ */
 Formato::Formato()
 {
-    nome= "PT-BR";
-    separadorData= '/';
-    separadorHorario= ':';
+    nome = "PT-BR";
+    separadorData = '/';
+    separadorHorario = ':';
 }
 
+/**
+ * @brief Construct a new Formato:: Formato object
+ *
+ * @param valNome
+ */
 Formato::Formato(string valNome)
 {
-    nome= valNome;
-    if (strcmp(valNome.c_str(),"PT-BR")==0)
-        separadorData= '/';
-    else if (strcmp(valNome.c_str(),"EN-US")==0)
-        separadorData= '-';
-    separadorHorario= ':';
+    nome = valNome;
+    if (strcmp(valNome.c_str(), "PT-BR") == 0)
+        separadorData = '/';
+    else if (strcmp(valNome.c_str(), "EN-US") == 0)
+        separadorData = '-';
+    separadorHorario = ':';
 }
 
-Formato::~Formato(){}
+/**
+ * @brief Destroy the Formato:: Formato object
+ *
+ */
+Formato::~Formato() {}
 
+/**
+ * @brief Retorna o nome do formato
+ *
+ * @return string
+ */
 string Formato::getNome()
 {
     return nome;
 }
 
+/**
+ * @brief Retorna o separador de data
+ *
+ * @return string
+ */
 string Formato::getSeparadorData()
 {
     return separadorData;
 }
 
+/**
+ * @brief Retorna o separador de horario
+ *
+ * @return string
+ */
 string Formato::getSeparadorHorario()
 {
     return separadorHorario;
