d2232c3c7ec2c1fcf573df8554d7fd3a0923b54b
EduScf <eduardoscf1210@gmail.com>
2022-11-14 15:48:32 -0300

    docs: doxygen estoque.cpp
    Implementação do doxygen da classe Estoque

--- src/estoque.cpp
+++ src/estoque.cpp
@@ -2,15 +2,28 @@
 
 #include "estoque.hpp"
 
+/**
+ * @brief Construct a new Estoque:: Estoque object
+ *
+ */
 Estoque::Estoque()
 {
 }
 
+/**
+ * @brief Destroy the Estoque:: Estoque object
+ *
+ */
 Estoque::~Estoque()
 {
 }
 
-Estoque* Estoque::getEstoque()
+/**
+ * @brief Retorna o estoque caso exista, senão o cria
+ *
+ * @return Estoque*
+ */
+Estoque *Estoque::getEstoque()
 {
     if (estoque == nullptr)
     {
@@ -19,44 +32,89 @@ Estoque* Estoque::getEstoque()
     return estoque;
 }
 
+/**
+ * @brief Adiciona um produto ao estoque
+ *
+ * @param produto
+ */
 void Estoque::addProduto(Produto produto)
 {
     produtos[produto.getCodigo()] = produto;
 }
 
+/**
+ * @brief Remove um produto do estoque
+ *
+ * @param codigo
+ */
 void Estoque::removeProduto(int codigo)
 {
     produtos.erase(codigo);
 }
 
+/**
+ * @brief Retorna um produto do estoque
+ *
+ * @param codigo
+ * @return Produto
+ */
 Produto Estoque::getProduto(int codigo)
 {
     return produtos[codigo];
 }
 
+/**
+ * @brief Retorna o estoque de produtos
+ *
+ * @return std::map<int, Produto>
+ */
 std::map<int, Produto> Estoque::getProdutos()
 {
     return produtos;
 }
 
+/**
+ * @brief Seta o estoque de produtos
+ *
+ * @param produtos
+ */
 void Estoque::setProdutos(std::map<int, Produto> produtos)
 {
     this->produtos = produtos;
 }
 
+/**
+ * @brief Valida a quantidade de estoquee mínimo
+ *
+ * @param codigo
+ */
 void Estoque::validaEstoqueMinimo(int codigo)
 {
     if (produtos[codigo].getQtdEstoque() < produtos[codigo].getEstoqueMinimo())
     {
-        std::cout << "Estoque minimo atingido" << std::endl;//lancar uma excecao aqui 
+        std::cout << "Estoque minimo atingido" << std::endl; // lancar uma excecao aqui
     }
 }
 
+/**
+ * @brief atualiza o estoque
+ *
+ * @param codigo
+ * @param quantidade
+ */
 void Estoque::atualizaEstoque(int codigo, int quantidade)
 {
     produtos[codigo].setQtdEstoque(produtos[codigo].getQtdEstoque() + quantidade);
 }
 
+/**
+ * @brief Verifica se o produto está disponivel no estoque
+ *
+ * @param codigo
+ * @param quantidade
+ * @return true
+ * @return false
+ */
 bool Estoque::disponivel(int codigo, int quantidade)
 {
     if (produtos[codigo].getQtdEstoque() >= quantidade)
@@ -65,9 +123,8 @@ bool Estoque::disponivel(int codigo, int quantidade)
     }
     else
     {
-        return false;//lancar excecao aqui
+        return false; // lancar excecao aqui
     }
 }
 
-Estoque* Estoque::estoque = nullptr;
-
+Estoque *Estoque::estoque = nullptr;
