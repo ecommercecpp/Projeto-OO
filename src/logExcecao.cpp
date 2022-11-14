a44d186f8dd712a14a45c7bd3381bd9e1207aa02
EduScf <eduardoscf1210@gmail.com>
2022-11-14 16:04:46 -0300

    docs: doxygen logExcecao.cpp
    Implementação do doxygen da classe logExcecao

--- src/logExcecao.cpp
+++ src/logExcecao.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 
-
 #include "logExcecao.hpp"
 
-LogExcecao::LogExcecao(Usuario usuario, Data data, std::string entidade, std::string funcionalidade): Logs(usuario, data, entidade)
+/**
+ * @brief Construct a new Log Excecao:: Log Excecao object
+ *
+ * @param usuario
+ * @param data
+ * @param entidade
+ * @param funcionalidade
+ */
+LogExcecao::LogExcecao(Usuario usuario, Data data, std::string entidade, std::string funcionalidade) : Logs(usuario, data, entidade)
 {
     this->funcionalidade = funcionalidade;
 }
 
+/**
+ * @brief Construct a new Log Excecao:: Log Excecao object
+ *
+ */
 LogExcecao::LogExcecao()
 {
 }
 
+/**
+ * @brief Destroy the Log Excecao:: Log Excecao object
+ *
+ */
 LogExcecao::~LogExcecao()
 {
 }
@@ -22,21 +37,41 @@ LogExcecao::~LogExcecao()
     return log;
 }*/
 
+/**
+ * @brief Seta o historico do log de exceção
+ *
+ * @param historicoLogExcecao
+ */
 void LogExcecao::setHistoricoLogExcecao(std::vector<std::string> historicoLogExcecao)
 {
     this->historicoLogExcecao = historicoLogExcecao;
 }
 
+/**
+ * @brief Retorna o historico do log de exceção
+ *
+ * @return std::vector<std::string>
+ */
 std::vector<std::string> LogExcecao::getHistoricoLogExcecao()
 {
     return this->historicoLogExcecao;
 }
 
+/**
+ * @brief Seta a funcionalidade da exceção
+ *
+ * @param funcionalidade
+ */
 void LogExcecao::setFuncionalidade(std::string funcionalidade)
 {
     this->funcionalidade = funcionalidade;
 }
 
+/**
+ * @brief Retorna a funcionalidade da exceção
+ *
+ * @return std::string
+ */
 std::string LogExcecao::getFuncionalidade()
 {
     return this->funcionalidade;
