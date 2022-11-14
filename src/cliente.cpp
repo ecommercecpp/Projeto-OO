83cb82cb31f0aaec9b44ac1165ee3ef7d53574ff
EduScf <eduardoscf1210@gmail.com>
2022-11-14 13:42:41 -0300

    docs: doxygen cliente.cpp
    Implementação Doxygen da classe Cliente

--- src/cliente.cpp
+++ src/cliente.cpp
@@ -3,10 +3,24 @@
 #include "cliente.hpp"
 #include "exceptions.hpp"
 
+/**
+ * @brief Construct a new Cliente:: Cliente object
+ *
+ */
 Cliente::Cliente()
 {
 }
 
+/**
+ * @brief Construct a new Cliente:: Cliente object
+ *
+ * @param nome
+ * @param cpf
+ * @param endereco
+ * @param telefone
+ * @param email
+ * @param dataNascimento
+ */
 Cliente::Cliente(std::string nome, std::string cpf_cnpj, std::string endereco, std::string email, int tipo, std::string telefone) : Pessoa(nome, cpf_cnpj, endereco, email, tipo)
 {
 	this->nome = nome;
@@ -15,16 +29,26 @@ Cliente::Cliente(std::string nome, std::string cpf_cnpj, std::string endereco, s
 	this->email = email;
 	this->tipo = tipo;
 	this->telefone = telefone;
-	//setTelefone(telefone);
+	// setTelefone(telefone);
 }
 
+/**
+ * @brief Retorna o Telefone do cliente
+ *
+ * @return string
+ */
 std::string Cliente::getTelefone()
 {
 	return telefone;
 }
 
+/**
+ * @brief Seta o Telefone do cliente
+ *
+ * @param telefone
+ */
 void Cliente::setTelefone(std::string t)
-{ 
+{
 	// valida se o telefone tem entre 10 e 11 digitos
 	if (t.length() >= 10 && t.length() <= 11)
 	{
@@ -32,7 +56,7 @@ void Cliente::setTelefone(std::string t)
 	}
 	else
 	{
-		std::cout<<"Telefone invalido!"<<std::endl;	
+		std::cout << "Telefone invalido!" << std::endl;
 		throw InvalidTelefoneException();
 	}
 }
