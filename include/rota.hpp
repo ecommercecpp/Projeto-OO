2242eff5c45b52c6031c28d824b73b81d56347b7
Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
2022-11-28 16:09:27 -0300

    feat: Adicionando coisas da sprint 7

--- include/rota.hpp
+++ include/rota.hpp
@@ -2,32 +2,57 @@
 #define _ROTA_HPP_
 
 #include <string>
+#include <iostream>
+#include <vector>
+#include <math>
 
 #include "exceptions.hpp"
 #include "funcionario.hpp"
+#include "empresa.hpp"
 #include "turno.hpp"
 
 
 class Rota
 {
-    private:
-        Funcionario Funcionario;
-        int Id;
-        float longChegada;
-        float latChegada;
-        std::string horaChegada;
-        std::string horaSaida;
-        std::string tempoRota;
-        turno Turno;
+private:
+    Funcionario Funcionario;
+    int Id;
+    /*float longChegada;
+    float latChegada;
+    std::string horaChegada;
+    std::string horaSaida;
+    std::string tempoRota;*/
+    Turno turno;
 
-    public:
-        Rota();
-        Rota(Rota &other) = delete;
-        Rota& operator=(Rota &other) = delete;
-        ~Rota();
-        float calculaTempo();
-        float calculaDistancia(float lat, float long);
-
-     
+public:
+    Rota()
+    {
+        std::string permissao = "cadastrarRota";
+        std::string atributos = "funcionario, id, longChegada, latChegada, horaChegada, horaSaida, tempoRota, turno";
+        Empresa::getEmpresa()->gerarLogEscrita("rota", atributos);
+        if (!Empresa::getEmpresa()->verificaPermissao(permissao))
+        {
+            Empresa::getEmpresa()->gerarLogExcecao("rota", "Rota");
+            throw AcessDeniedException();
+        }
+        else
+        {
+        
+        }
+    }
+    ~Rota() {}
+    /**
+     * @brief Verifica se o produto está disponivel no estoque
+     *
+     * @param x1
+     * @param x2
+     * @param y1
+     * @param y2
+     * @return true
+     */
+    float calculaDistancia(float x1, float y1, float x2, float y2)
+    {
+        return 110.57 * sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    }
 };
 #endif
--- include/veiculo.hpp
+++ include/veiculo.hpp
@@ -6,30 +6,119 @@
 
 #include "exceptions.hpp"
 #include "funcionario.hpp"
-
+#include "empresa.hpp"
 
 class Veiculo
 {
-    private:
-        int tipo;
-        int capacidade;
-        //std::string placa;
-        std::vector <Funcionario> rotas;
+private:
+    int tipo;
+    int capacidade;
+    // std::string placa;
+    std::vector<Funcionario> rotas;
 
-    public:
-        Veiculo();
-        Veiculo(Veiculo &other) = delete;
-        Veiculo& operator=(Veiculo &other) = delete;
-        ~Veiculo();
-        void alteraHorario(std::string horario);
-        int getCapacidade();
-        //std::vector<Funcionario*> getRotas();
-        void setCapacidade(int capacidade);
-        //void setRotas(std::vector<Funcionario*>); 
-        void setTipo(int tipo);
-        int getTipo();
-        std::string retornaTipoVeiculo();
-     
+public:
+    Veiculo()
+    {
+    }
+
+    /**
+     * @brief Destroy the Veiculo:: Veiculo object
+     *
+     */
+    ~Veiculo()
+    {
+    }
+
+    /**
+     * @brief Seta o tipo do veiculo
+     *
+     * @param tipo
+     */
+    void setTipo(int tipo)
+    {
+        std::string permissao = "setarTipoVeiculo";
+        std::string atributos = "tipo";
+        Empresa::getEmpresa()->gerarLogEscrita("veiculo", atributos);
+        if (!Empresa::getEmpresa()->verificaPermissao(permissao))
+        {
+            Empresa::getEmpresa()->gerarLogExcecao("veiculo", "setTipo");
+            throw AcessDeniedException();
+        }
+        else
+        {
+            this->tipo = tipo;
+        }
+    }
+
+    /**
+     * @brief Retorna o tipo do veiculo
+     *
+     * @return std::string
+     */
+    int getTipo()
+    {
+        std::string permissao = "verificarTipoVeiculo";
+        std::string informacao = "tipo";
+        Empresa::getEmpresa()->gerarLogLeitura("veiculo", informacao);
+        if (!Empresa::getEmpresa()->verificaPermissao(permissao))
+        {
+            Empresa::getEmpresa()->gerarLogExcecao("veiculo", "getTipo");
+            throw AcessDeniedException();
+        }
+        else
+        {
+            return this->tipo;
+        }
+    }
+
+    /**
+     * @brief retorna o tipo do veiculo (onibus ou micro-onibus)
+     *
+     * @param void
+     */
+    std::string retornaTipoVeiculo()
+    {
+        std::string permissao = "retornarTipoVeiculo";
+        std::string informacao = "tipo";
+        Empresa::getEmpresa()->gerarLogLeitura("veiculo", informacao);
+        if (!Empresa::getEmpresa()->verificaPermissao(permissao))
+        {
+            Empresa::getEmpresa()->gerarLogExcecao("veiculo", "retornaTipoVeiculo");
+            throw AcessDeniedException();
+        }
+        else
+        {
+            if (this->tipo == 0)
+            {
+                return "onibus";
+            }
+            else
+            {
+                return "micro-onibus";
+            }
+        }
+    }
+
+    /**
+     * @brief Altera o horario de saida do veiculo
+     *
+     * @param horario
+     */
+    void alteraHorario(std::string horario)
+    {
+        std::string permissao = "alterarHorarioVeiculo";
+        std::string atributos = "horario: " + horario;
+        Empresa::getEmpresa()->gerarLogEscrita("veiculo", atributos);
+        if (!Empresa::getEmpresa()->verificaPermissao(permissao))
+        {
+            Empresa::getEmpresa()->gerarLogExcecao("veiculo", "alteraHorario");
+            throw AcessDeniedException();
+        }
+        else
+        {
+            // this->horario = horario;
+        }
+    }
 };
 
 #endif
