94bb3db1ab83349a6524837127a2bfe4fdbb6bbf
Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
2022-11-27 21:20:18 -0300

    ordem de compra

--- include/ordemCompra.hpp
+++ include/ordemCompra.hpp
@@ -1,7 +1,9 @@
 #ifndef _ORDEM_COMPRA_HPP_
 #define _ORDEM_COMPRA_HPP_
 
+#include <iostream>
 #include <string>
+#include <ctime>
 
 #include "Data.h"
 #include "lote.hpp"
@@ -15,19 +17,23 @@ class OrdemCompra
 		std::string dataString;
 		int quantidade;
 	public:
-		OrdemCompra();
-		//OrdemProducao(Lote lote, Data data, int quantidade);
-		~OrdemCompra();
-        void setNomeMP(std::string nomeMP);
-        std::string getNomeMP();
-        void setData(Data data);
-        Data getData();
-        int getQuantidade();
-        void setQuantidade(int quantidade);
-        void setDataString(std::string dataString);
-        std::string getDataString();
-
-		void gerarOrdemCompra(MateriaPrima*, int);
+		OrdemCompra(){};
+		~OrdemCompra(){};
+		void gerarOrdemCompra(MateriaPrima* mp, int quantidade){
+                Lote lote;
+                Data data;
+                time_t tt;
+                struct tm * ti;
+                time (&tt);
+                ti = localtime(&tt); 
+                
+                std::cout << "Ordem de compra: " << std::endl;
+                this->nomeMP = mp->getNome();
+                this->quantidade = quantidade;
+                this->dataDaOrdem = data.dateNow();
+                this->dataString = asctime(ti);
+                std::cout<<"Ordem de compra gerada com sucesso!"<<std::endl;
+        }
 };
 
 #endif
